04434c08ead75da62898bb258329cc61344e64c0
oscarLIMc <oscar.islas@mccollect.mc>
2025-11-10 12:40:54 -0600

    fix: corregir dirección IP en el mensaje de inicio del servidor

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3000;
     await sequelize.sync({ alter: true });
 
     app.listen(PORT, () =>
-      console.log(`✅ Server running on http://10.55.37.8:${PORT}`)
+      console.log(`✅ Server running on http://10.55.37.2:${PORT}`)
     );
   } catch (e) {
     console.error("❌ Error starting server:", e);
